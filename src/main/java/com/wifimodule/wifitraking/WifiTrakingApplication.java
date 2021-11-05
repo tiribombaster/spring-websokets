942f442cc692c927ae23d2232b4ebc55e30c101c
tiribombaster <traian.rusu92@gmail.com>
2021-11-06 00:50:29 +0200

    initial commit

---
+++ src/main/java/com/wifimodule/wifitraking/WifiTrakingApplication.java
@@ -0,0 +1,13 @@
+package com.wifimodule.wifitraking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiTrakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiTrakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wifimodule/wifitraking/WifiTrakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wifimodule.wifitraking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiTrakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
